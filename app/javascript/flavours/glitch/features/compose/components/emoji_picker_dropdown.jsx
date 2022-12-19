0558ad9dbfe6b85a865074567691c4bb2a285b82
neatchee <neatchee@gmail.com>
2022-12-18 19:35:31 -0800

    Embiggen the emoji picker!
    Whitespace fix

--- app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
+++ app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
@@ -272,8 +272,8 @@ class EmojiPickerMenuImpl extends React.PureComponent {
       <div className={classNames('emoji-picker-dropdown__menu', { selecting: modifierOpen })} style={style} ref={this.setRef}>
         <EmojiPicker
           perLine={8}
-          emojiSize={22}
-          sheetSize={32}
+          emojiSize={33}
+          sheetSize={43}
           custom={buildCustomEmojis(custom_emojis)}
           color=''
           emoji=''
