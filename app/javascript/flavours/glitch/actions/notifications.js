e2d00446f23e07350730604674d6c56f92fd53fd
Essem <smswessem@gmail.com>
2023-11-07 18:43:47 -0600

    Add support for emoji reactions Squashed, modified, and rebased from glitch-soc/mastodon#2221.
    Co-authored-by: fef <owo@fef.moe>
    Co-authored-by: Jeremy Kescher <jeremy@kescher.at>
    Co-authored-by: neatchee <neatchee@gmail.com>
    Co-authored-by: Ivan Rodriguez <104603218+IRod22@users.noreply.github.com>
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/glitch/actions/notifications.js
+++ app/javascript/flavours/glitch/actions/notifications.js
@@ -170,6 +170,7 @@ const excludeTypesFromFilter = filter => {
     'follow',
     'follow_request',
     'favourite',
+    'reaction',
     'reblog',
     'mention',
     'poll',
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -27,6 +27,7 @@ class Notification < ApplicationRecord
     'Follow' => :follow,
     'FollowRequest' => :follow_request,
     'Favourite' => :favourite,
+    'StatusReaction' => :reaction,
     'Poll' => :poll,
   }.freeze
 
@@ -47,6 +48,9 @@ class Notification < ApplicationRecord
       filterable: true,
     }.freeze,
     favourite: {
+      filterable: true,
+    }.freeze,
+    reaction: {
       filterable: true,
     }.freeze,
     poll: {
@@ -76,6 +80,7 @@ class Notification < ApplicationRecord
     reblog: [status: :reblog],
     mention: [mention: :status],
     favourite: [favourite: :status],
+    reaction: [status_reaction: :status],
     poll: [poll: :status],
     update: :status,
     'admin.report': [report: :target_account],
@@ -91,6 +96,7 @@ class Notification < ApplicationRecord
     belongs_to :follow, inverse_of: :notification
     belongs_to :follow_request, inverse_of: :notification
     belongs_to :favourite, inverse_of: :notification
+    belongs_to :status_reaction, inverse_of: :notification
     belongs_to :poll, inverse_of: false
     belongs_to :report, inverse_of: false
     belongs_to :account_relationship_severance_event, inverse_of: false
@@ -113,6 +119,8 @@ class Notification < ApplicationRecord
       status&.reblog
     when :favourite
       favourite&.status
+    when :reaction
+      status_reaction&.status
     when :mention
       mention&.status
     when :poll
@@ -237,6 +245,8 @@ class Notification < ApplicationRecord
     end
   end
 
+  alias reaction status_reaction
+
   after_initialize :set_from_account
   before_validation :set_from_account
 
@@ -248,7 +258,7 @@ class Notification < ApplicationRecord
     return unless new_record?
 
     case activity_type
-    when 'Status', 'Follow', 'Favourite', 'FollowRequest', 'Poll', 'Report'
+    when 'Status', 'Follow', 'Favourite', 'StatusReaction', 'FollowRequest', 'Poll', 'Report'
       self.from_account_id = activity&.account_id
     when 'Mention'
       self.from_account_id = activity&.status&.account_id
