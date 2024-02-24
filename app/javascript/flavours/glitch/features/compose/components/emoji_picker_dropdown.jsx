ec79078518fec2873388a2ee1fda6b1070d8dfc8
Essem <smswessem@gmail.com>
2024-02-24 17:20:38 -0600

    Fix reaction picker dropdown appearance

--- app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
+++ app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
@@ -323,6 +323,8 @@ class EmojiPickerDropdown extends PureComponent {
     onPickEmoji: PropTypes.func.isRequired,
     onSkinTone: PropTypes.func.isRequired,
     skinTone: PropTypes.number.isRequired,
+    title: PropTypes.string,
+    icon: PropTypes.node,
     disabled: PropTypes.bool,
   };
 
@@ -386,8 +388,8 @@ class EmojiPickerDropdown extends PureComponent {
   };
 
   render() {
-    const { intl, onPickEmoji, onSkinTone, skinTone, frequentlyUsedEmojis, disabled } = this.props;
-    const title = intl.formatMessage(messages.emoji);
+    const { intl, onPickEmoji, onSkinTone, skinTone, frequentlyUsedEmojis, title: propsTitle, icon, disabled } = this.props;
+    const title = propsTitle ?? intl.formatMessage(messages.emoji);
     const { active, loading, placement } = this.state;
 
     return (
@@ -396,7 +398,7 @@ class EmojiPickerDropdown extends PureComponent {
           title={title}
           aria-expanded={active}
           active={active}
-          iconComponent={MoodIcon}
+          iconComponent={icon || MoodIcon}
           onClick={this.onToggle}
           disabled={disabled}
           id="emoji"
