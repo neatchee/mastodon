5750d82e3939ab5fa2e457d3e464d64468280c9a
neatchee <neatchee@gmail.com>
2024-10-18 16:07:03 -0700

    Quote modal bugfix

--- app/javascript/flavours/glitch/features/ui/components/modal_root.jsx
+++ app/javascript/flavours/glitch/features/ui/components/modal_root.jsx
@@ -67,7 +67,7 @@ export const MODAL_COMPONENTS = {
   'CONFIRM_UNFOLLOW': () => Promise.resolve({ default: ConfirmUnfollowModal }),
   'CONFIRM_CLEAR_NOTIFICATIONS': () => Promise.resolve({ default: ConfirmClearNotificationsModal }),
   'CONFIRM_LOG_OUT': () => Promise.resolve({ default: ConfirmLogOutModal }),
-  'CONFIRM_QUOTE': () => Promise.resolve({ dfeault: ConfirmQuoteModal }),
+  'CONFIRM_QUOTE': () => Promise.resolve({ default: ConfirmQuoteModal }),
   'MUTE': MuteModal,
   'BLOCK': BlockModal,
   'DOMAIN_BLOCK': DomainBlockModal,
