e4edc133faf2ca812c81a7e5a01a86d7abd2db34
Essem <smswessem@gmail.com>
2024-12-04 20:42:34 -0600

    Add bubble domain list to about page

--- app/javascript/flavours/glitch/features/about/index.jsx
+++ app/javascript/flavours/glitch/features/about/index.jsx
@@ -8,7 +8,7 @@ import { Helmet } from 'react-helmet';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
 
-import { fetchServer, fetchExtendedDescription, fetchDomainBlocks  } from 'flavours/glitch/actions/server';
+import { fetchServer, fetchExtendedDescription, fetchDomainBlocks, fetchBubbleDomains  } from 'flavours/glitch/actions/server';
 import { Account } from 'flavours/glitch/components/account';
 import Column from 'flavours/glitch/components/column';
 import { ServerHeroImage } from 'flavours/glitch/components/server_hero_image';
@@ -21,6 +21,7 @@ import { RulesSection } from './components/rules';
 const messages = defineMessages({
   title: { id: 'column.about', defaultMessage: 'About' },
   blocks: { id: 'about.blocks', defaultMessage: 'Moderated servers' },
+  bubble: { id: 'about.bubble.title', defaultMessage: 'Bubble servers' },
   silenced: { id: 'about.domain_blocks.silenced.title', defaultMessage: 'Limited' },
   silencedExplanation: { id: 'about.domain_blocks.silenced.explanation', defaultMessage: 'You will generally not see profiles and content from this server, unless you explicitly look it up or opt into it by following.' },
   suspended: { id: 'about.domain_blocks.suspended.title', defaultMessage: 'Suspended' },
@@ -44,6 +45,7 @@ const mapStateToProps = state => ({
   locale: state.getIn(['meta', 'locale']),
   extendedDescription: state.getIn(['server', 'extendedDescription']),
   domainBlocks: state.getIn(['server', 'domainBlocks']),
+  bubbleDomains: state.getIn(['server', 'bubbleDomains']),
 });
 
 class About extends PureComponent {
@@ -53,6 +55,11 @@ class About extends PureComponent {
     locale: ImmutablePropTypes.string,
     extendedDescription: ImmutablePropTypes.map,
     domainBlocks: ImmutablePropTypes.contains({
+      isLoading: PropTypes.bool,
+      isAvailable: PropTypes.bool,
+      items: ImmutablePropTypes.list,
+    }),
+    bubbleDomains: ImmutablePropTypes.contains({
       isLoading: PropTypes.bool,
       isAvailable: PropTypes.bool,
       items: ImmutablePropTypes.list,
@@ -73,8 +80,13 @@ class About extends PureComponent {
     dispatch(fetchDomainBlocks());
   };
 
+  handleBubbleDomainsOpen = () => {
+    const { dispatch } = this.props;
+    dispatch(fetchBubbleDomains());
+  };
+
   render () {
-    const { multiColumn, intl, server, extendedDescription, domainBlocks, locale } = this.props;
+    const { multiColumn, intl, server, extendedDescription, domainBlocks, bubbleDomains, locale } = this.props;
     const isLoading = server.get('isLoading');
 
     return (
@@ -124,6 +136,28 @@ class About extends PureComponent {
           </Section>
 
           <RulesSection />
+
+          <Section title={intl.formatMessage(messages.bubble)} onOpen={this.handleBubbleDomainsOpen}>
+            {bubbleDomains.get('isLoading') ? (
+              <Skeleton width='100%' />
+            ) : (bubbleDomains.get('isAvailable') ? (
+              <>
+                <p><FormattedMessage id='about.bubble.preamble' defaultMessage='This server provides a "bubble timeline", which displays content from these other servers in the fediverse that have been chosen by the admins of this server.' /></p>
+
+                {bubbleDomains.get('items').size > 0 && (
+                  <div className='about__bubble-domains'>
+                    {bubbleDomains.get('items').map(domain => (
+                      <div className='about__bubble-domains__domain' key={domain}>
+                        <h6 className='about__bubble-domains__domain__header'>{domain}</h6>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </>
+            ) : (
+              <p><FormattedMessage id='about.not_available' defaultMessage='This information has not been made available on this server.' /></p>
+            ))}
+          </Section>
 
           <Section title={intl.formatMessage(messages.blocks)} onOpen={this.handleDomainBlocksOpen}>
             {domainBlocks.get('isLoading') ? (
