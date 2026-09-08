f7c84bb0dcbd6ec90d0926de7c0bc6cb728a97c9
Essem <smswessem@gmail.com>
2026-09-08 12:38:25 -0500

    Fix connectBubbleStream return type

--- app/javascript/flavours/glitch/actions/streaming.js
+++ app/javascript/flavours/glitch/actions/streaming.js
@@ -187,7 +187,7 @@ export const connectCommunityStream = ({ onlyMedia } = {}) =>
 /**
  * @param {Object} options
  * @param {boolean} [options.onlyMedia]
- * @returns {() => void}
+ * @returns {StreamThunk}
  */
 export const connectBubbleStream = ({ onlyMedia } = {}) =>
   connectTimelineStream(`bubble${onlyMedia ? ':media' : ''}`, `public:bubble${onlyMedia ? ':media' : ''}`, {}, {
