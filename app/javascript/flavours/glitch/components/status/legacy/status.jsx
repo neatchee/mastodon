08289220718cb0473141e28df268eb3017fd0fcc
neatchee <neatchee@gmail.com>
2026-09-21 22:33:07 -0700

    Fix duplicate status reaction bar from bad merge

--- app/javascript/flavours/glitch/components/status/legacy/status.jsx
+++ app/javascript/flavours/glitch/components/status/legacy/status.jsx
@@ -785,11 +785,6 @@ class Status extends ImmutablePureComponent {
             {/* This is a glitch-soc addition to have a placeholder */}
             {!expanded && <MentionsPlaceholder status={status} />}
 
-            <StatusReactions
-              id={status.get('id')}
-              reactions={status.get('reactions').toArray()}
-            />
-
             {(showActions && !isQuotedPost) &&
               <StatusReactions
                 id={status.get('id')}
